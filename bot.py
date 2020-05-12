2fd9fd59308539fd31f24aab7d795e70abd08d69
Xalion <xalion67@gmail.com>
2020-05-12 16:01:08 +0300

    add loger

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
 import telebot as tb
 import spotify
+import logging
 from to_json import to_json
+import proxy_changer
 
 
+proxy = proxy_changer.read_proxy()
 bot = tb.TeleBot('940145749:AAENwzTWDnBkbCXwJZ8Fw7XdS0GCM5CgZoU', threaded=False)
+tb.apihelper.proxy = proxy_changer.set_proxy(proxy)
 spotify_client = spotify.Spotify()
 track_data = {}
+# настройка логера
+bot_logger = logging.getLogger('bot')
+logging.basicConfig(filename='bot.log', level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%d.%m.%Y %H:%M')
 
 
 @bot.message_handler(commands=['start'])
@@ -26,9 +35,13 @@ def search_artist(message):
         artist = spotify_client.search_artist(message.text)
         if artist is None:
             bot.send_message(chat_id=message.chat.id, text='Ничего не нашел')
+            bot_logger.info('Artist "{}" not found, user – {}\n'.format(message.text,
+                                                                    message.from_user.username))
         else:
             bot.send_photo(chat_id=message.chat.id, photo=artist.pic,
                            caption=artist.name, reply_markup=artist.button)
+            bot_logger.info('Artist "{}" found, user – {}\n'.format(message.text,
+                                                                    message.from_user.username))
 
 
 @bot.message_handler(func=lambda message: message.text.lower() == 'поиск трека')
@@ -40,18 +53,34 @@ def search_track(message):
     track = spotify_client.search_track(message.text)
     if track is None:
         bot.send_message(chat_id=message.chat.id, text='Ничего не нашел')
+        bot_logger.info('Track "{}" not found, user – {}\n'.format(message.text,
+                                                                   message.from_user.username))
     else:
         bot.send_photo(chat_id=message.chat.id, photo=track.pic_album,
                        caption=track.caption, reply_markup=track.keyboard, parse_mode='markdown')
         global track_data
-        track_data[track.id] = track.preview_url
+        track_data[track.id] = track
+        bot_logger.info('Track "{}" found, user – {}\n'.format(message.text,
+                                                               message.from_user.username))
 
 
 @bot.callback_query_handler(func=lambda query: True)
 def query_handler(query):
     if query.data in track_data.keys():
-        bot.send_audio(chat_id=query.message.chat.id, audio=track_data[query.data])
+        track = track_data[query.data]
+        bot.send_audio(chat_id=query.message.chat.id, audio=track.preview_url,
+                       caption=track.name)
+        bot_logger.info('Track preview was sent')
+    else:
+        bot.send_message(query.message.chat.id, 'Не могу найти этот трек у себя')
+        bot_logger.warning('Track preview wasn`t sent, not found id')
 
 
-bot.polling()
+try:
+    bot.polling()
+except OSError:
+    bot.stop_polling()
+    proxy = proxy_changer.get_proxy()
+    proxy_changer.write_proxy(proxy)
+    bot.polling()
 
